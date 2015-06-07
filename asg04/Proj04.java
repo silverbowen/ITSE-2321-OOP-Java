5a3753d6acbfbd5dd4417f3ec6f9cff73164d2fb
silverbowen <silver.quickness@gmail.com>
2015-06-07 09:08:44 -0500

    initial asg04 commit

---
+++ asg04/Proj04.java
@@ -0,0 +1,12 @@
+//34567890123456789012345678901234567890123456789012345678
+/*File Proj04 Copyright 2008 R.G.Baldwin
+*********************************************************/
+import java.awt.Color;
+public class Proj04{
+  //DO NOT MODIFY THE CODE IN THIS CLASS DEFINITION.
+  public static void main(String[] args){
+    Picture pic = new Proj04Runner().run();
+    System.out.println(pic);
+  }//end main method
+}//end class Proj04
+//End program specifications.
